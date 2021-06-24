f45f978cbf960bb0eba7bf93a8cbf71289391299
Anthony Almanza <anthonya771@gmail.com>
2021-06-24 14:30:59 -0700

    intial commit

---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+const { colors, fonts } = require("../../config")
+
+export const sharedPreferences = {
+  fonts: fonts,
+  breakpoints: {
+    xxs: "320px",
+    xs:"375px",
+    sm: "768px",
+    md: "992px",
+    lg: "1200px",
+  },
+  borderRadius: "1rem",
+  pageWidth: "62.5rem",
+  headerHeight: "6.25rem",
+  footerHeight: "7.5rem",
+}
+
+export const lightTheme = {
+  ...sharedPreferences,
+  colors: colors.lightTheme,
+}
+
+export const darkTheme = {
+  ...sharedPreferences,
+  colors: colors.darkTheme,
+}
